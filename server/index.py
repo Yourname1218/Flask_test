8b866c3d9ee5d7915c177d5dd36c167e4df2e6b8
Yourname1218 <101972412+Yourname1218@users.noreply.github.com>
2023-12-04 00:48:22 +0800

    Add files via upload

--- server/index.py
+++ server/index.py
@@ -3,23 +3,18 @@ import subprocess
 
 app = Flask(__name__)
 
-# 主頁面
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# 更新主機名稱的功能
 @app.route('/update_hostname', methods=['POST'])
 def update_hostname():
     if request.method == 'POST':
         new_hostname = request.form['new_hostname']
-        
-        # 執行命令更新主機名稱
         subprocess.call(['hostnamectl', 'set-hostname', new_hostname])
-        
-        return f'主機名稱已更新為 {new_hostname}'
+        return f'Hostname updated to {new_hostname}'
     else:
-        return '只允許 POST 請求'
+        return 'Only POST requests are allowed'
 
 if __name__ == '__main__':
     app.run(debug=True)
